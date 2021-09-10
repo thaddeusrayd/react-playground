313fab4bfb2c38239290fd6be0d92900ad8d1fe7
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-10 09:42:18 -0500

    track email field state

--- src/SubscriberForm.js
+++ src/SubscriberForm.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 function SubscriberForm() {
   const [name, setName] = useState("");
   const handleNameChange = (event) => setName(event.target.value);
+  const [email, setEmail] = useState("");
+  const handleEmailChange = (event) => setEmail(event.target.value);
 
   console.log("Current value of name:", name);
+  console.log("Current value of email:", name);
   return (
     <form>
       <label htmlFor="name">
@@ -17,7 +22,13 @@ function SubscriberForm() {
       </label>
       <label htmlFor="email">
         Your Email:
-        <input id="email" type="email" name="email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          onChange={handleEmailChange}
+          value={email}
+        />
       </label>
     </form>
   );
