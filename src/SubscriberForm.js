893b5151cbcff0e859818bef217d9d55bfdd210b
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-10 10:03:21 -0500

    finish form and add subView and subList comps

--- src/SubscriberForm.js
+++ src/SubscriberForm.js
@@ -5,11 +5,17 @@ function SubscriberForm() {
   const handleNameChange = (event) => setName(event.target.value);
   const [email, setEmail] = useState("");
   const handleEmailChange = (event) => setEmail(event.target.value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted:", name, email);
+    setName("");
+    setEmail("");
+  };
 
   console.log("Current value of name:", name);
   console.log("Current value of email:", name);
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="name">
         Enter Your Name:
         <input
@@ -30,6 +36,7 @@ function SubscriberForm() {
           value={email}
         />
       </label>
+      <button type="submit">Submit</button>
     </form>
   );
 }
