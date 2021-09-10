8e4af1921293bc71c3d599a86e18271962cd548f
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-10 10:23:03 -0500

    prep App for comps and write SubList doc

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { useState } from "react";
 import SubscriberForm from "./SubscriberForm";
+import SubscriberList from "./SubscriberList";
 
 function App() {
-  return <SubscriberForm />;
+  const [subscribers, setSubscribers] = useState([]);
+  const createSubscriber = (newSubscriber) =>
+    setSubscribers((currentSubscribers) => [
+      newSubscriber,
+      ...currentSubscribers,
+    ]);
+  const deleteSubscriber = (indexToDelete) =>
+    setSubscribers((currentSubscribers) =>
+      currentSubscribers.filter((post, index) => index !== indexToDelete)
+    );
+
+  return (
+    <>
+      <SubscriberForm createSubscriber={createSubscriber} />
+      <SubscriberList
+        deleteSubscriber={deleteSubscriber}
+        subscribers={subscribers}
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/SubscriberList.js
@@ -0,0 +1,18 @@
+import SubscriberView from "./SubscriberView";
+
+function SubscriberList({ deleteSubscriber, subscribers }) {
+  return (
+    <>
+      <h2>Subscribers</h2>
+      <ul>
+        {subscribers.map((subscriber, index) => (
+          <subscriberView
+            deleteSubscriber={() => deleteSubscriber(index)}
+            key={index}
+            subscriber={subscriber}
+          />
+        ))}
+      </ul>
+    </>
+  );
+}
